6d705a810b895c2ee26b53205c190fcbad8482ec
Jared Callaham <jared.callaham@gmail.com>
2025-03-24 08:59:21 -0400

    Initial code

--- src/archimedes/__init__.py
+++ src/archimedes/__init__.py
@@ -1,2 +1,59 @@
-def hello() -> str:
-    return "Hello from archimedes!"
+from . import error
+
+from .core import (
+    codegen,
+    array,
+    sym,
+    sym_like,
+    sym_function,
+    zeros,
+    ones,
+    zeros_like,
+    ones_like,
+    eye,
+    scan,
+    jac,
+    grad,
+    hess,
+    jvp,
+    vjp,
+    interpolant,
+)
+
+from . import tree
+from .tree import struct
+
+from .simulation import integrator, odeint
+from .optimization import nlp_solver, minimize, implicit, root
+
+
+__all__ = [
+    "error",
+    "tree",
+    "struct",
+    "codegen",
+    "array",
+    "core",
+    "sym",
+    "sym_like",
+    "zeros",
+    "ones",
+    "zeros_like",
+    "ones_like",
+    "eye",
+    "scan",
+    "sym_function",
+    "grad",
+    "jac",
+    "hess",
+    "jvp",
+    "vjp",
+    "interpolant",
+    "integrator",
+    "odeint",
+    "nlp_solver",
+    "minimize",
+    "implicit",
+    "root",
+    "experimental",
+]
