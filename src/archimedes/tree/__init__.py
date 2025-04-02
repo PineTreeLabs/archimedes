9ddc846f6449e7782211c8b193a9adb32a7fe5ad
Jared Callaham <jared.callaham@gmail.com>
2025-04-02 06:03:43 -0400

    Move copyright back to block comment

--- src/archimedes/tree/__init__.py
+++ src/archimedes/tree/__init__.py
@@ -1,35 +1,37 @@
-"""
-This file contains code modified from pybaum
-Original Copyright (c) 2022 Janoś Gabler, Tobias Raabe
-https://github.com/OpenSourceEconomics/pybaum
+"""Utilities for working with hierarchical "pytree" data structures."""
 
-The original code has been modified throughout. While the original code remains
-under its MIT license, all modifications and additions are:
-Copyright (c) 2025 Jared Callaham
-Licensed under the GNU General Public License v3.0
+# TODO: Did this file actually end up using any code from pybaum?
 
-As a combined work, use of this code requires compliance with the GNU GPL v3.0.
-The original MIT license terms are included below for attribution:
+# This file contains code modified from pybaum
+# Original Copyright (c) 2022 Janoś Gabler, Tobias Raabe
+# https://github.com/OpenSourceEconomics/pybaum
 
-MIT LICENSE
+# The original code has been modified throughout. While the original code remains
+# under its MIT license, all modifications and additions are:
+# Copyright (c) 2025 Jared Callaham
+# Licensed under the GNU General Public License v3.0
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this
-software and associated documentation files (the "Software"), to deal in the Software
-without restriction, including without limitation the rights to use, copy, modify,
-merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to the following
-conditions:
+# As a combined work, use of this code requires compliance with the GNU GPL v3.0.
+# The original MIT license terms are included below for attribution:
 
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
+# MIT LICENSE
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
-OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE."
-"""
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to the following
+# conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies
+# or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from . import struct
 from ._flatten_util import ravel_pytree as ravel
