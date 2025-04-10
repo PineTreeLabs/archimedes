c42aef2ac90c69a63dc0bca2052fdbc0020c003e
Jared Callaham <jared.callaham@gmail.com>
2025-04-10 06:36:45 -0400

    Fix experimental.__init__

--- src/archimedes/experimental/__init__.py
+++ src/archimedes/experimental/__init__.py
@@ -1,6 +1,5 @@
 from . import aero, coco, fluid, state_estimation, sysid, thermo
 from .lqr import lqr_design
-from .modred import balanced_truncation
 
 __all__ = [
     "Coco",
@@ -10,5 +9,4 @@ __all__ = [
     "state_estimation",
     "sysid",
     "lqr_design",
-    "balanced_truncation",
 ]
