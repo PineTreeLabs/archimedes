f6739dce1bcf09c29dfd854ca0f7aeb0f63c9f0c
Jared Callaham <jared.callaham@gmail.com>
2025-04-16 19:34:50 -0400

    qpsol -> conic

--- test/optimization/test_qpsol.py
+++ test/optimization/test_qpsol.py
@@ -13,48 +13,22 @@ class TestQPSolve:
             return x[0] + x[1] - 10
 
         x0 = np.array([0.0, 0.0])
-        sol = arc.qpsol(f, g, x0, lba=0)
+        sol = arc.qpsol(f, g, x0, lbg=0)
 
         # Check against the values from the CasADi example
         assert np.allclose(sol.x, [5.0, 5.0], atol=1e-2)
         assert np.allclose(sol.lam_a, -10, atol=1e-2)
 
         # Test with warm starting
-        sol2 = arc.qpsol(f, g, x0, lba=0, lam_a0=sol.lam_a)
+        sol2 = arc.qpsol(f, g, x0, lbg=0, lam_a0=sol.lam_a)
         assert np.allclose(sol2.x, sol.x, atol=1e-2)
         assert np.allclose(sol2.lam_a, sol.lam_a, atol=1e-2)
 
-    def test_with_args(self):
-        def f(x, a):
-            return a * x[0] ** 2 + x[1] ** 2
-
-        def g(x, a):
-            return x[0] + x[1] - 10
-
-        x0 = np.array([0.0, 0.0])
-        sol = arc.qpsol(f, g, x0, lba=0, args=(1.0,))
-
-        # Check against the values from the CasADi example
-        assert np.allclose(sol.x, [5.0, 5.0], atol=1e-2)
-        assert np.allclose(sol.lam_a, -10, atol=1e-2)
-
         # Test with no bounds
-        sol = arc.qpsol(f, g, x0, args=(1.0,))
+        sol = arc.qpsol(f, g, x0)
         assert np.allclose(sol.x, [0, 0], atol=1e-2)
         assert np.allclose(sol.lam_a, 0, atol=1e-2)
-        # print(sol)
 
-        # # Test warm starting
-        # sol2 = qpsol(
-        #     f,
-        #     g,
-        #     sol.x,
-        #     lba=0,
-        #     lam_x0=sol.lam_x,
-        #     lam_a0=sol.lam_a,
-        #     check_termination=5,
-        #     verbose=True,
-        # )
 
     def test_error_handling(self):
         # Test inconsistent signatures
@@ -65,7 +39,7 @@ class TestQPSolve:
             return x[0] + x[1] - 10
 
         with pytest.raises(ValueError, match=r".*same number of arguments.*"):
-            arc.qpsol(f, g, x0=[0.0, 0.0], lba=0)
+            arc.qpsol(f, g, x0=[0.0, 0.0], lbg=0)
 
         # Test inconsistent static arguments
         def f(a, x):
@@ -78,7 +52,7 @@ class TestQPSolve:
         g = arc.compile(g)
 
         with pytest.raises(ValueError, match=r".*same number of static arguments.*"):
-            arc.qpsol(f, g, x0=[0.0, 0.0], lba=0)
+            arc.qpsol(f, g, x0=[0.0, 0.0], lbg=0)
 
         # Test inconsistent decision variables type
         def f(x):
@@ -88,4 +62,4 @@ class TestQPSolve:
             return x[0] + x[1] - 10
 
         with pytest.raises(ValueError, match=r".*Only scalar and vector.*"):
-            arc.qpsol(f, g, x0=np.zeros((2, 2)), lba=0)
+            arc.qpsol(f, g, x0=np.zeros((2, 2)), lbg=0)
