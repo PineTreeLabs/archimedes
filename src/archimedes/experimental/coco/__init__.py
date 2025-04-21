e32c5e0c740faa54796f9baf936d27ac7527c56a
Jared Callaham <jared.callaham@gmail.com>
2025-04-21 05:31:13 -0400

    Remove Hermite-Simpson

--- src/archimedes/experimental/coco/__init__.py
+++ src/archimedes/experimental/coco/__init__.py
@@ -3,4 +3,3 @@ from .discretization import *
 from .interpolation import *
 from .mesh_refinement import *
 from .ocp import *
-from .hermite_simpson import HermiteSimpsonCollocation, UniformTimeGrid
