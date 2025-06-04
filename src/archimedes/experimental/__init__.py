e5c5073f4a87702cd9c83baed2007c25f0e120bd
Jared Callaham <jared.callaham@gmail.com>
2025-06-04 03:08:15 -0400

    Add ContinuousEKF back in experimental

--- src/archimedes/experimental/__init__.py
+++ src/archimedes/experimental/__init__.py
@@ -1,10 +1,11 @@
-from . import aero, coco, signal
+from . import aero, coco, observers, signal
 from .lqr import lqr_design
 from .balanced_truncation import balanced_truncation
 
 __all__ = [
     "coco",
     "aero",
+    "observers",
     "signal",
     "lqr_design",
     "balanced_truncation",
