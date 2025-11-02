71ddf5e1ad337ca92caee48a25b4f604fe07f015
Jared Callaham <jared.callaham@gmail.com>
2025-11-02 05:53:04 -0500

    Add spatial test files

---
+++ test/spatial/test_euler.py
@@ -0,0 +1,43 @@
+# ruff: noqa: N802, N803, N806
+
+import numpy as np
+import pytest
+from scipy.spatial.transform import Rotation as ScipyRotation
+
+import archimedes as arc
+from archimedes.spatial import euler_to_dcm
+
+np.random.seed(0)
+
+
+def random_euler():
+    """Generate a random rotation"""
+    return np.random.randn(3)
+
+
+def test_euler_to_dcm():
+    angles = random_euler()
+    for seq in ("XYZ", "xyz", "ZYX", "zyx", "XYX", "zxz"):
+        print(seq)
+        R1 = euler_to_dcm(angles, seq=seq)
+        R2 = ScipyRotation.from_euler(seq, angles).as_matrix()
+
+        np.testing.assert_allclose(R1, R2)
+
+
+def test_error_handling():
+    # Invalid axis sequence
+    with pytest.raises(ValueError, match="Expected axes from `seq`"):
+        euler_to_dcm([0.1, 0.2, 0.3], "abc")
+
+    # Invalid euler shape
+    with pytest.raises(ValueError, match="Expected axis specification to be"):
+        euler_to_dcm([0.1, 0.2, 0.3], "xyzyz")
+
+    # Angles shape doesn't match sequence
+    with pytest.raises(ValueError, match="For xyz sequence with 3 axes, `angles`"):
+        euler_to_dcm([0.1, 0.2], "xyz")
+
+    # Repeated axis in sequence
+    with pytest.raises(ValueError, match="Expected consecutive axes"):
+        euler_to_dcm([0.1, 0.2, 0.3], "xxz")
