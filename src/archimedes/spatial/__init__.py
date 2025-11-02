04fd464895a4575d8e9178e8164d95e007520cd7
Jared Callaham <jared.callaham@gmail.com>
2025-11-02 05:34:03 -0500

    Remove _attitude subdirectory

--- src/archimedes/spatial/__init__.py
+++ src/archimedes/spatial/__init__.py
@@ -4,19 +4,22 @@ from ._rigid_body import (
     RigidBody,
     RigidBodyConfig,
 )
-from ._attitude import (
-    Rotation,
+from ._euler import (
     euler_kinematics,
     euler_to_dcm,
+)
+from ._quaternion import (
+    dcm_to_quaternion,
     euler_to_quaternion,
-    quaternion_kinematics,
     quaternion_multiply,
+    quaternion_kinematics,
     quaternion_to_dcm,
     quaternion_to_euler,
 )
-
+from ._rotation import Rotation
 
 __all__ = [
+    "dcm_to_quaternion",
     "euler_kinematics",
     "euler_to_dcm",
     "euler_to_quaternion",
