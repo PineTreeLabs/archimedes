6b9419e41309c17337dcfb51b974a3b2905b184f
Jared Callaham <jared.callaham@gmail.com>
2026-02-27 12:34:21 -0500

    Remove NDArray entirely

--- src/archimedes/typing.py
+++ src/archimedes/typing.py
@@ -3,18 +3,14 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, Tuple, Union
 
 import casadi as cs
+import numpy as np
 from numpy.typing import DTypeLike
-
-try:
-    from numpy.typing import NDArray
-except ImportError:  # pragma: no cover  # numpy <1.21 lacks NDArray
-    from numpy import ndarray as NDArray  # type: ignore[assignment]
 from typing_extensions import TypeAlias
 
 if TYPE_CHECKING:
     from ._core import SymbolicArray
 
-    ArrayLike: TypeAlias = Union[NDArray, SymbolicArray]
+    ArrayLike: TypeAlias = Union[np.ndarray, SymbolicArray]
 
 
 Tree: TypeAlias = Any
@@ -24,7 +20,6 @@ CasadiMatrix: TypeAlias = Union[cs.SX, cs.MX, cs.DM]
 ShapeLike: TypeAlias = Tuple[int, ...]
 
 __all__ = [
-    "NDArray",
     "ArrayLike",
     "Tree",
     "DTypeLike",
